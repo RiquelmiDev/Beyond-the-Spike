9b151538dcfafdcb4b3887fa4b290b5276b84a8f
Riquelmi Santos <riquelme.dos.santos.kw@gmail.com>
2024-09-19 12:23:51 -0300

    Icones dos personagens iseridos dinamicamente e alteracoes na logica

--- src/JS/insereDados.js
+++ src/JS/insereDados.js
@@ -20,8 +20,8 @@ apiResponse().then(agentes => {
                 habilidades: agente.abilities
             })
         });
+        
         criarElementosDosPersonagens(dataAgentes);
-/*         inserirDados(dataAgentes); */
     }
 });
 
@@ -30,16 +30,38 @@ const imgPersonagem = document.getElementById("personagemImg");
 const nomePersonagem = document.getElementById("nomePersonagem");
 const rolePersonagem = document.getElementById("rolePersonagem");
 
-const listaPersonagens = document.getElementById("listaPersonagens");
+
 
 function criarElementosDosPersonagens (dataAgentes) {
 
+    
+    adicionarImagensPersonagensAoCarrousel(dataAgentes);
+    
+    adicionarIconesPersonagens(dataAgentes);
+    /*  nomePersonagem.textContent = dataAgentes[24].nomeAgente;
+    descricao.textContent = dataAgentes[24].descricaoAgente;
+    imgPersonagem.src = dataAgentes[24].imgAgente; */
+
+    //Incia o cod responsavel pelo carrousel
+    initSplide();
+
+}
+
+
+
+//Funcao responsavel por criar as tags li com as imagens de cada agente do valorant
+function adicionarImagensPersonagensAoCarrousel(dataAgentes){
+    //elemento ul que guarda as imagens
+    const listaPersonagens = document.getElementById("listaPersonagens");
+
     let output= "";
 
-    dataAgentes.forEach(element => {
+    // ideia repentina: achar uma forma de adicionar as li individualmente e logo apos chamar a funcao de add background mandando o background do elemento atual a cada iteracao dentro do for fazendo com que nao precisace de um for dentro da funcao de bg
+
+    dataAgentes.forEach(personagemAtual => {
         output += 
         `<li class="splide__slide">       
-            <img class="personagemImg backgroundPersonagem${element.idAgente}" id="personagemImg${element.idAgente}" src="${element.imgAgente}" alt="Imagem do Personagem ${element.nomeAgente}" title="Personagem ${element.nomeAgente} do jogo Valorant"></li>`;
+            <img class="personagemImg backgroundPersonagem${personagemAtual.idAgente}" id="personagemImg${personagemAtual.idAgente}" src="${personagemAtual.imgAgente}" alt="Imagem do Personagem ${personagemAtual.nomeAgente}" title="Imagem do personagem ${personagemAtual.nomeAgente} do jogo Valorant"></li>`;
     });
 
    // Criar um elemento temporário para converter a string em nós do DOM
@@ -53,19 +75,35 @@ function criarElementosDosPersonagens (dataAgentes) {
 
    //depois de inserir as li com as imagens no html ele insere o estilo que adiciona o background dos personagens no css
    adicionaBackgroundDasImagens(dataAgentes);
-
-   /*  nomePersonagem.textContent = dataAgentes[24].nomeAgente;
-    descricao.textContent = dataAgentes[24].descricaoAgente;
-    imgPersonagem.src = dataAgentes[24].imgAgente; */
-
-    //Incia o cod responsavel pelo carrousel
-    initSplide();
-
 }
 
 function adicionaBackgroundDasImagens(dataAgentes){
+
     dataAgentes.forEach(element => {
     let imagemDeFundoPersonagem = document.querySelector(`.backgroundPersonagem${element.idAgente}`);
         imagemDeFundoPersonagem.setAttribute('style', `background-image: url("${element.background}");background-repeat: no-repeat;background-size: contain; background-position:right`)
     });
 }
+
+function adicionarIconesPersonagens(dataAgentes){
+    const listaDeIconesPersonagens = document.getElementById("thumbnails");
+    let output= "";
+
+    dataAgentes.forEach(personagemAtual => {
+        output += 
+        `<li class="thumbnail">
+              <img src="${personagemAtual.displayIcon}" alt="Icone selecionavel do personagem ${personagemAtual.nomeAgente} title="Icone selecionavel personagem ${personagemAtual.nomeAgente}">
+        </li>`;
+    });
+
+   // Criar um elemento temporário para converter a string em nós do DOM
+   const tempContainer = document.createElement("div");
+   tempContainer.innerHTML = output;
+
+   // Adicionar os nós ao elemento da lista
+   while (tempContainer.firstChild) {
+    listaDeIconesPersonagens.appendChild(tempContainer.firstChild);
+   }
+}
+
+
